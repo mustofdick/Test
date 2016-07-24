845472e71706f12658884f516721ca36897afa60
Mihail Simbirsky <simbirsky@bk.ru>
2016-07-24 12:28:33 +0300

    q

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -6,8 +6,6 @@ package Application;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
-            System.out.println("Hello World!!!");
-        // sd
         }
     }
 
