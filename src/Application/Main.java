e181f9d704ce2e95caf56cbc1e1d23d776f2e431
norikoff <yuranorikov@gmail.com>
2016-07-24 14:47:06 +0300

    2Xsout

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -6,5 +6,8 @@ package Application;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+            System.out.println("Hello World!!!");
+        }
     }
-}
+
+
